7b5f1e0a600a113f79368daa2b79da75a8dbc498
gatt33 <129215899+gatt33@users.noreply.github.com>
2023-05-08 15:56:22 -0400

    New Monday

--- web_app/models/product.py
+++ web_app/models/product.py
@@ -1,12 +1,12 @@
 
 DEFAULT_PRODUCTS = [
     # see: https://picsum.photos/images
-    {'id': 1, 'name': 'Strawberries', 'description': 'Juicy organic strawberries.', 'price': 4.99, 'url': 'https://picsum.photos/id/1080/360/200'},
-    {'id': 2, 'name': 'Cup of Tea', 'description': 'An individually-prepared tea or coffee of choice.', 'price': 3.49, 'url': 'https://picsum.photos/id/225/360/200'},
-    {'id': 3, 'name': 'Textbook', 'description': 'It has all the answers.', 'price': 129.99, 'url': 'https://picsum.photos/id/24/360/200'},
-    #{'id': 4, 'name': 'Online Course', 'description': 'Level up your programming skills.', 'price': 129.99, 'url': 'https://picsum.photos/id/180/360/200'},
-    #{'id': 5, 'name': '________', 'description': '___________.', 'price': 0.99, 'url': '__________'},
-    #{'id': 6, 'name': '________', 'description': '___________.', 'price': 0.99, 'url': '__________'},
+    {'id': 1, 'name': 'Competition Prep', 'description': 'Bodybuilding Competition Training', 'price': 4.99, 'url': 'https://i.ibb.co/mHMB6p7/Bodybuilding-Prep-Training.jpg'},
+    {'id': 2, 'name': 'Cross Fit', 'description': 'An individually-prepared schedule for you.', 'price': 3.49, 'url': 'https://i.ibb.co/GJnLMyY/Cross-Fit-Training.jpg'},
+    {'id': 3, 'name': 'Nutrition Training', 'description': 'Get Lean or Thick with our experts', 'price': 129.99, 'url': 'https://i.ibb.co/xXSNkFK/Nutrition-Training.jpg'},
+    {'id': 4, 'name': 'Personal Training', 'description': 'We got you for the busy professionals', 'price': 129.99, 'url': 'https://i.ibb.co/xg317s0/personal-trainer-1-compressor.jpg'},
+    {'id': 5, 'name': 'Spin Training', 'description': 'Welcome for all age groups', 'price': 0.99, 'url': 'https://i.ibb.co/fXt8PTV/Spin-Training.jpg'},
+    {'id': 6, 'name': 'HIIT training', 'description': 'See results fast with us', 'price': 0.99, 'url': 'https://i.ibb.co/K9832Zq/HIIT-Training.jpg'}
 ]
 
 
